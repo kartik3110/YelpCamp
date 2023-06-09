16f549904740e8156238a05b28795a63fec81d24
kartik3110 <kartik31.gupta@gmail.com>
2023-06-09 16:22:52 +0530

    added some basic crud functionality

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const path = require('path');
+const methodOverride = require('method-override');
+
+const Campground = require('./models/campground');
 const app = express();
-const Campground = require('./models/campground')
-
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'));
-
-app.listen(3000, () => {
-    console.log('server running at port 3000');
-})
 
 mongoose.set('strictQuery', true);
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
@@ -21,9 +16,57 @@ db.once("open", () => {
 });
 
 
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'));
+
+
 app.get('/', (req, res) => {
-    res.send('hi');
+    res.send('this is the root page');
+})
+
+app.get('/campgrounds', async (req, res) => {
+    const allCamps = await Campground.find({});
+    res.render('campgrounds/index', { allCamps });
+})
+
+//create
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+})
+
+app.post('/campgrounds', async (req, res) => {
+    const newCamp = new Campground(req.body.campground);
+    await newCamp.save();
+    res.redirect(`/campgrounds/${newCamp.id}`)
+})
+
+//show
+app.get('/campgrounds/:id', async (req, res) => {
+    const id = req.params.id;
+    const foundCamp = await Campground.findById(id);
+    res.render('campgrounds/show', { foundCamp });
 })
 
 
 
+//edit
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const id = req.params.id;
+    const foundCamp = await Campground.findById(id);
+    res.render('campgrounds/edit', { foundCamp });
+})
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const id = req.params.id;
+    const newCamp = await Campground.findByIdAndUpdate(id, req.body.campground);
+    res.redirect(`/campgrounds/${newCamp.id}`)
+})
+
+
+
+app.listen(3000, () => {
+    console.log('server running at port 3000');
+})
